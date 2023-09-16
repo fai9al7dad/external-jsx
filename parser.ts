fa0274821152b97b8f9e8a0ce8b5cf55fadfc8a3
faisal haddad <63120233+fai9al7dad@users.noreply.github.com>
2023-09-17 01:29:10 +0300

    Add files via upload

--- parser.ts
+++ parser.ts
@@ -7,7 +7,6 @@ const templateFileExtension = `.template.${componentFileExtension}`;
 
 // this function will return the jsx markup for a given template file path
 function parseTemplateFile(filePath: string): string | null {
-
   const fileContents = fs.readFileSync(filePath, "utf-8");
   const sourceFile = ts.createSourceFile(
     filePath,
@@ -16,20 +15,28 @@ function parseTemplateFile(filePath: string): string | null {
     true
   );
 
-  let jsxMarkup: string | null = null;
+  // TODO:: Allow imports in template files -> possibly by assuming that everything before the first root node is an import
 
+  return parseJsx(fileContents, sourceFile);
+}
+
+function parseJsx(
+  fileContents: string,
+  sourceFile: ts.SourceFile
+): string | null {
   // TODO:: throw error if more than one root node is found
 
+  let jsxMarkup: string | null = null;
   function getRootNode(node: ts.Node): string | void {
     // parse until you find the first jsx element
     if (
-        ts.isJsxElement(node) ||
-        ts.isJsxSelfClosingElement(node) ||
-        ts.isJsxFragment(node)
+      ts.isJsxElement(node) ||
+      ts.isJsxSelfClosingElement(node) ||
+      ts.isJsxFragment(node)
     ) {
       // if found root then return and stop looping
       jsxMarkup = fileContents.substring(node.pos, node.end);
-      return
+      return;
     }
     ts.forEachChild(node, getRootNode);
   }
@@ -39,6 +46,9 @@ function parseTemplateFile(filePath: string): string | null {
   return jsxMarkup;
 }
 
+const isComponentFile = (file: string): boolean => {
+  return file.endsWith(".tsx") && !file.endsWith(templateFileExtension);
+};
 
 function parseDirectory(directoryPath: string): void {
   const files = fs.readdirSync(directoryPath);
@@ -48,30 +58,29 @@ function parseDirectory(directoryPath: string): void {
     if (stats.isDirectory()) {
       parseDirectory(filePath);
     } else if (stats.isFile()) {
-      if (file.endsWith(".tsx") && !file.endsWith(templateFileExtension)) {
-        console.log(`Parsing file ${filePath}`);
+      if (isComponentFile(file)) {
         const fileContents = fs.readFileSync(filePath, "utf-8");
         const templateTag = fileContents.match(
-            /<Template\s*dir\s*=\s*.*\s*\/>/,
-        )
-        if (templateTag){
-          let dirValue = templateTag[0].split(
-            /"|'.+"|'/,
-        )[1]
-          if (!dirValue.endsWith(templateFileExtension)){
-            new Error(`Template file ${dirValue} does not have the correct extension. Expected ${templateFileExtension}`)
+          /<Template\s*dir\s*=\s*.*\s*\/>/
+        );
+        if (templateTag) {
+          let dirValue = templateTag[0].split(/"|'.+"|'/)[1];
+          if (!dirValue.endsWith(templateFileExtension)) {
+            new Error(
+              `Template file ${dirValue} does not have the correct extension. Expected ${templateFileExtension}`
+            );
           }
-          if (dirValue.startsWith("/")){
-              dirValue = dirValue.substring(1)
+          if (dirValue.startsWith("/")) {
+            dirValue = dirValue.substring(1);
           }
           const templateFilePath = `${directoryPath}/${dirValue}`;
           const jsxMarkup = parseTemplateFile(templateFilePath);
           if (jsxMarkup) {
-              const componentCode = fileContents.replace(
-                  /<Template\s*dir\s*=\s*.*\s*\/>/,
-                  jsxMarkup,
-              );
-              fs.writeFileSync(filePath, componentCode);
+            const componentCode = fileContents.replace(
+              /<Template\s*dir\s*=\s*.*\s*\/>/,
+              jsxMarkup
+            );
+            fs.writeFileSync(filePath, componentCode);
           }
         }
       }
@@ -80,7 +89,6 @@ function parseDirectory(directoryPath: string): void {
 }
 
 parseDirectory("./example/src");
-
 
 //         const componentCode = fs
 //             .readFileSync(componentFilePath, "utf-8")
